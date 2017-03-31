cc16cfee8f3d4db58306472de37cbb6d95ba1c7e
Andrew S <andrews54757@gmail.com>
2017-03-31 09:58:39 -0400

    Update index.js

--- source/commands/index.js
+++ source/commands/index.js
@@ -1,1 +1,26 @@
-
+module.exports = {
+ details: [
+   [
+     name: "help",
+     description: "List available commands"
+     ],
+   [
+     name: "list"
+     description: "List players/bots"
+     
+     
+     ],
+   [
+     name: "kill",
+     description: "Kills a player",
+     options: [{
+     name: "pID",
+     description: "Player id to kill"
+     }]
+     
+     ]
+   
+   
+ ],
+  
+}
